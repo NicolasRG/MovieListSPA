38cb54f755bb9971f51cd80d8df392fe77a146bd
Nicolas Ramos Gomez <nico426@gmail.com>
2019-09-23 00:29:16 -0400

    Added mongo connection

--- express/server.js
+++ express/server.js
@@ -1,9 +1,25 @@
 // module imports 
 const express = require("express");
+const mongoose  = require("mongoose");
+
+
 
 //global server constants
+
 const app = express();
 const PORT = 80;
+
+mongoose.connect('mongodb://node:92219@nicoprojects.com/test', {useNewUrlParser: true});
+const db = mongoose.connection;
+const movie = db.movie;
+
+db.on('error', function(){
+    console.log("wers")
+});
+db.once('open', function() {
+  // we're connected!
+  console.log("Mongod Connected");
+});
 
 //logging
 app.use(function(req, res, next){
@@ -17,6 +33,11 @@ app.get('/', function(req,res){
 });
 
 app.get('/mongoDBtest', function(req, res){
+    try{
+        console.log(db);
+    }catch(e){
+        console.log(e, "we died");
+    }
     res.send(true+"");
 });
 
